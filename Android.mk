7b1a95c822abe7d1fd030a1c7eced0075e432b04
David <Davidteri91@gmail.com>
2017-02-14 01:28:51 +0100

    sony-common: use lineage project map system
    do not use TARGET_BOARD_AUTO
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- Android.mk
+++ Android.mk
@@ -81,25 +81,25 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-audio-hal := hardware/qcom/audio
-display-hal := hardware/qcom/display/msm8994
-gps-hal := hardware/qcom/gps/msm8994
-media-hal := hardware/qcom/media/msm8974
+#audio-hal := hardware/qcom/audio
+#display-hal := hardware/qcom/display/msm8994
+#gps-hal := hardware/qcom/gps/msm8994
+#media-hal := hardware/qcom/media/msm8974
 
-include $(display-hal)/Android.mk
-include $(call all-makefiles-under,$(audio-hal))
-include $(call all-makefiles-under,$(gps-hal))
-include $(call all-makefiles-under,$(media-hal))
+#include $(display-hal)/Android.mk
+#include $(call all-makefiles-under,$(audio-hal))
+#include $(call all-makefiles-under,$(gps-hal))
+#include $(call all-makefiles-under,$(media-hal))
 
-ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
-ifneq ($(filter rhine,$(PRODUCT_PLATFORM)),)
-  include $(call all-makefiles-under,hardware/qcom/bt/msm8960)
-else
-  include $(call all-makefiles-under,hardware/qcom/bt/msm8992)
-endif
-endif
-ifeq ($(BOARD_WLAN_DEVICE),qcwcn)
-  include $(call all-makefiles-under,hardware/qcom/wlan/qcwcn)
-endif
+#ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+#ifneq ($(filter rhine,$(PRODUCT_PLATFORM)),)
+#  include $(call all-makefiles-under,hardware/qcom/bt/msm8960)
+#else
+#  include $(call all-makefiles-under,hardware/qcom/bt/msm8992)
+#endif
+#endif
+#ifeq ($(BOARD_WLAN_DEVICE),qcwcn)
+#  include $(call all-makefiles-under,hardware/qcom/wlan/qcwcn)
+#endif
 
 endif
